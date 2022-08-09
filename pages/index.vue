db766c29cf82dc2e4314818275b64e8093993d53
Guillaume Roussin <guillaume.roussin44@gmail.com>
2022-08-09 08:46:01 +0200

    Initial Commit

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Hello world</h1>
+
+  <nuxt-content :document="page"></nuxt-content>
+</template>
+
+<script setup lang="ts">
+  const page = await getContent('home').fetch();
+</script>
